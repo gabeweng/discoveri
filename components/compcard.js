7a86a51dd87efeb2fa3a81a75fd8e42beb4d98b0
Gabriel Weng <gabeweng@gmail.com>
2023-08-10 13:17:14 -0400

    move image to top

--- components/compcard.js
+++ components/compcard.js
@@ -1,8 +1,13 @@
-import { Card, CardHeader, CardBody, CardFooter, Flex, Heading, Text} from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Flex, Heading, Text, Image, Button} from '@chakra-ui/react'
 
 function CompCard(props) {
     return (
         <Card maxW='md'>
+        <Image
+            objectFit='cover'
+            src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            alt='Chakra UI'
+        />
         <CardHeader>
             <Flex spacing='4'>
             <Heading size='md'>{props.info.name}</Heading>
@@ -13,12 +18,7 @@ function CompCard(props) {
             {props.info.description}
             </Text>
         </CardBody>
-        <Image
-            objectFit='cover'
-            src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-            alt='Chakra UI'
-        />
-
+    
         <CardFooter
             justify='space-between'
             flexWrap='wrap'
